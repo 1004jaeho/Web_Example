0f57f7d0717cce76797bb2950f3db1887e6bba42
1004jaeho <yooonjh333@gamil.com>
2019-08-07 17:32:19 +0900

    0807

--- DB/Db_info.js
+++ DB/Db_info.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-inner-declarations */
+/* eslint-disable no-use-before-define */
+/* eslint-disable padded-blocks */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prefer-const */
+/* eslint-disable camelcase */
+/* eslint-disable no-undef */
 /* eslint-disable no-continue */
 /* eslint-disable no-plusplus */
 /* eslint-disable func-names */
@@ -14,7 +21,6 @@
 // eslint-disable-next-line no-continue
 const mysql = require('mysql');
 
-
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -26,21 +32,33 @@ const connection = mysql.createConnection({
 connection.connect();
 
 
-function a (require, callback) {
-  connection.query('SELECT userid from user', (error, rows) => {
-    let userid = null;
+function a (require, callback) { 
+  connection.query('SELECT userid,password from user', (error, rows) => {
     if (error) throw error;
-    console.log('userid : ', rows);
+    console.log('rows : ', rows);
     for (let i = 0; i < rows.length; i++) {
-      userid = rows[i];
-      if (userid === rows[i]) {
+      db_userid = rows[i].userid;
+      db_password = rows[i].password;
+      console.log(`db_userid : ${db_userid} db_password : ${db_password}`);
+      set(db_userid, db_password);
+      
+      if (db_userid === rows[i].userid && db_password === rows[i].db_password) {
         callback('success');
       } else {
-        callback('fail');
+
+        callback('fail'); 
       }
+      
+      
     }
+    
   });
   connection.end();
 }
+
+function set (db_userid, db_password) { 
+  console.log(` set 함수 db_userid : ${db_userid} db_password : ${db_password}`);
+}
+
 
 module.exports = a;
--- router/Node_Main.js
+++ router/Node_Main.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable padded-blocks */
+/* eslint-disable camelcase */
+/* eslint-disable import/order */
 /* eslint-disable no-undef */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-console */
@@ -7,6 +11,7 @@ const http = require('http');
 const fs = require('fs'); // 파일 읽기, 쓰기 등 을 할 수 있는 모듈
 const url = require('url');
 const a = require('../DB/Db_info');
+const set = require('../DB/Db_info');
 const querystring = require('querystring');
 
 // 404 error message : 페이지 오류가 발생했을때,
@@ -25,18 +30,28 @@ function onRequest(request, response) {
     fs.createReadStream('./Main.html').pipe(response); // 같은 디렉토리에 있는 index.html를 response 함
     post(request);
     request.on('data', (chunk) => {
-      console.log(chunk.toString());
       const data = querystring.parse(chunk.toString());
+      console.log(data);
+      get(data.userid, data.password);
       response.writeHead(200, { 'Content-Type': 'text/html' }); // header 설정
-      response.end(`id : ${  data.userid  } pw : ${  data.password}`);
+      response.end(`id : ${data.userid} pw : ${data.password}`);
     });
-    
   } else {
     response.send404Message = 404;
     // file이 존재 하지않을때,
     send404Message(response);
   }
 }
+
+
+//  데이터 저장
+function get(userid, password) {
+  const cm_userid = userid;
+  const cm_password = password;  
+  console.log(cm_userid, cm_password);
+  //  여기에서 값을 비교할것입니다.
+}
+
 
 function post(request, response) {
   const path = url.parse(request.url, true).pathname; // url에서 path 추출
